81b98dc7f5cfdf5344ead11418c91dfcac8e5efe
Andre Eisenbach <git@4ae.us>
2024-12-29 11:38:44 -0500

    Day 17: JIT - Documentation

--- 17/day_17_jit.cc
+++ 17/day_17_jit.cc
@@ -58,11 +58,11 @@ TEST(Day_17_Chronospatial_Computer_JIT_ADV_Register_B) {
 }
 
 TEST(Day_17_Chronospatial_Computer_JIT_ADV_Register_C) {
-  const auto div_ac = std::vector<uint8_t>{0, 5, 5, 4};
-  EXPECT_EQ(out({0}), jit(div_ac)(7U, 3U, 0U));
-  EXPECT_EQ(out({3}), jit(div_ac)(7U, 1U, 0U));
-  EXPECT_EQ(out({3}), jit(div_ac)(25U, 3U, 0U));
-  EXPECT_EQ(out({4}), jit(div_ac)(4'096U, 10U, 0U));
+  const auto div_ac = std::vector<uint8_t>{0, 6, 5, 4};
+  EXPECT_EQ(out({0}), jit(div_ac)(7U, 0U, 3U));
+  EXPECT_EQ(out({3}), jit(div_ac)(7U, 0U, 1U));
+  EXPECT_EQ(out({3}), jit(div_ac)(25U, 0U, 3U));
+  EXPECT_EQ(out({4}), jit(div_ac)(4'096U, 0U, 10U));
 }
 
 //
@@ -79,6 +79,45 @@ TEST(Day_17_Chronospatial_Computer_JIT_BXL) {
 }
 
 //
+// OPCODE 2 - BST
+//
+
+TEST(Day_17_Chronospatial_Computer_JIT_BST_Literal) {
+  const auto bst_0 = std::vector<uint8_t>{2, 0, 5, 5};
+  const auto bst_1 = std::vector<uint8_t>{2, 1, 5, 5};
+  const auto bst_2 = std::vector<uint8_t>{2, 2, 5, 5};
+  const auto bst_3 = std::vector<uint8_t>{2, 3, 5, 5};
+  EXPECT_EQ(out({0}), jit(bst_0)(0U, 0U, 0U));
+  EXPECT_EQ(out({1}), jit(bst_1)(0U, 0U, 0U));
+  EXPECT_EQ(out({2}), jit(bst_2)(0U, 0U, 0U));
+  EXPECT_EQ(out({3}), jit(bst_3)(0U, 0U, 0U));
+}
+
+TEST(Day_17_Chronospatial_Computer_JIT_BST_Register_A) {
+  const auto bst_a = std::vector<uint8_t>{2, 4, 5, 5};
+  EXPECT_EQ(out({0}), jit(bst_a)(0U, 0U, 0U));
+  EXPECT_EQ(out({3}), jit(bst_a)(3U, 0U, 0U));
+  EXPECT_EQ(out({7}), jit(bst_a)(7U, 0U, 0U));
+  EXPECT_EQ(out({2}), jit(bst_a)(10U, 0U, 0U));
+}
+
+TEST(Day_17_Chronospatial_Computer_JIT_BST_Register_B) {
+  const auto bst_b = std::vector<uint8_t>{2, 5, 5, 5};
+  EXPECT_EQ(out({0}), jit(bst_b)(0U, 0U, 0U));
+  EXPECT_EQ(out({3}), jit(bst_b)(0U, 3U, 0U));
+  EXPECT_EQ(out({7}), jit(bst_b)(0U, 7U, 0U));
+  EXPECT_EQ(out({2}), jit(bst_b)(0U, 10U, 0U));
+}
+
+TEST(Day_17_Chronospatial_Computer_JIT_BST_Register_C) {
+  const auto bst_c = std::vector<uint8_t>{2, 6, 5, 5};
+  EXPECT_EQ(out({0}), jit(bst_c)(0U, 0U, 0U));
+  EXPECT_EQ(out({3}), jit(bst_c)(0U, 0U, 3U));
+  EXPECT_EQ(out({7}), jit(bst_c)(0U, 0U, 7U));
+  EXPECT_EQ(out({2}), jit(bst_c)(0U, 0U, 10U));
+}
+
+//
 // OPCODE 3 - JNZ
 //
 
@@ -91,6 +130,20 @@ TEST(Day_17_Chronospatial_Computer_JIT_JNZ_Jump_Forward) {
   const auto src = std::vector<uint8_t>{3, 2, 5, 5, 5, 6};
   EXPECT_EQ(out({1, 2}), jit(src)(0U, 1U, 2U));
   EXPECT_EQ(out({2}), jit(src)(1U, 1U, 2U));
+}
+
+//
+// OPCODE 4 - BXC
+//
+
+TEST(Day_17_Chronospatial_Computer_JIT_BXC) {
+  const auto bxc = std::vector<uint8_t>{4, 0, 5, 5};
+  EXPECT_EQ(out({0}), jit(bxc)(0U, 0U, 0U));
+  EXPECT_EQ(out({0}), jit(bxc)(0U, 7U, 7U));
+  EXPECT_EQ(out({1}), jit(bxc)(0U, 1U, 0U));
+  EXPECT_EQ(out({1}), jit(bxc)(0U, 0U, 1U));
+  EXPECT_EQ(out({3}), jit(bxc)(0U, 2U, 1U));
+  EXPECT_EQ(out({5}), jit(bxc)(0U, 1U, 4U));
 }
 
 //
@@ -115,6 +168,96 @@ TEST(Day_17_Chronospatial_Computer_JIT_OUT_Registers_Mod8) {
 }
 
 //
+// OPCODE 6 - BDV
+//
+
+TEST(Day_17_Chronospatial_Computer_JIT_BDV_Literal) {
+  const auto div_1 = std::vector<uint8_t>{6, 0, 5, 5};
+  EXPECT_EQ(out({7}), jit(div_1)(7U, 0U, 0U));
+  EXPECT_EQ(out({0}), jit(div_1)(0U, 0U, 0U));
+
+  const auto div_2 = std::vector<uint8_t>{6, 1, 5, 5};
+  EXPECT_EQ(out({3}), jit(div_2)(7U, 0U, 0U));
+  EXPECT_EQ(out({0}), jit(div_2)(0U, 0U, 0U));
+
+  const auto div_4 = std::vector<uint8_t>{6, 2, 5, 5};
+  EXPECT_EQ(out({1}), jit(div_4)(7U, 0U, 0U));
+  EXPECT_EQ(out({0}), jit(div_4)(0U, 0U, 0U));
+
+  const auto div_8 = std::vector<uint8_t>{6, 3, 5, 5};
+  EXPECT_EQ(out({3}), jit(div_8)(24U, 0U, 0U));
+  EXPECT_EQ(out({0}), jit(div_8)(7U, 0U, 0U));
+}
+
+TEST(Day_17_Chronospatial_Computer_JIT_BDV_Register_A) {
+  const auto div_aa = std::vector<uint8_t>{6, 4, 5, 5, 5, 4};
+  EXPECT_EQ(out({0, 7}), jit(div_aa)(7U, 0U, 0U));
+  EXPECT_EQ(out({0, 0}), jit(div_aa)(0U, 0U, 0U));
+  EXPECT_EQ(out({0, 0}), jit(div_aa)(1'024U, 0U, 0U));
+}
+
+TEST(Day_17_Chronospatial_Computer_JIT_BDV_Register_B) {
+  const auto div_ab = std::vector<uint8_t>{6, 5, 5, 5, 5, 4};
+  EXPECT_EQ(out({0, 7}), jit(div_ab)(7U, 3U, 0U));
+  EXPECT_EQ(out({3, 7}), jit(div_ab)(7U, 1U, 0U));
+  EXPECT_EQ(out({3, 1}), jit(div_ab)(25U, 3U, 0U));
+  EXPECT_EQ(out({4, 0}), jit(div_ab)(4'096U, 10U, 0U));
+}
+
+TEST(Day_17_Chronospatial_Computer_JIT_BDV_Register_C) {
+  const auto div_ac = std::vector<uint8_t>{6, 6, 5, 5, 5, 4};
+  EXPECT_EQ(out({0, 7}), jit(div_ac)(7U, 0U, 3U));
+  EXPECT_EQ(out({3, 7}), jit(div_ac)(7U, 0U, 1U));
+  EXPECT_EQ(out({3, 1}), jit(div_ac)(25U, 0U, 3U));
+  EXPECT_EQ(out({4, 0}), jit(div_ac)(4'096U, 0U, 10U));
+}
+
+//
+// OPCODE 7 - CDV
+//
+
+TEST(Day_17_Chronospatial_Computer_JIT_CDV_Literal) {
+  const auto div_1 = std::vector<uint8_t>{7, 0, 5, 6};
+  EXPECT_EQ(out({7}), jit(div_1)(7U, 0U, 0U));
+  EXPECT_EQ(out({0}), jit(div_1)(0U, 0U, 0U));
+
+  const auto div_2 = std::vector<uint8_t>{7, 1, 5, 6};
+  EXPECT_EQ(out({3}), jit(div_2)(7U, 0U, 0U));
+  EXPECT_EQ(out({0}), jit(div_2)(0U, 0U, 0U));
+
+  const auto div_4 = std::vector<uint8_t>{7, 2, 5, 6};
+  EXPECT_EQ(out({1}), jit(div_4)(7U, 0U, 0U));
+  EXPECT_EQ(out({0}), jit(div_4)(0U, 0U, 0U));
+
+  const auto div_8 = std::vector<uint8_t>{7, 3, 5, 6};
+  EXPECT_EQ(out({3}), jit(div_8)(24U, 0U, 0U));
+  EXPECT_EQ(out({0}), jit(div_8)(7U, 0U, 0U));
+}
+
+TEST(Day_17_Chronospatial_Computer_JIT_CDV_Register_A) {
+  const auto div_aa = std::vector<uint8_t>{7, 4, 5, 6, 5, 4};
+  EXPECT_EQ(out({0, 7}), jit(div_aa)(7U, 0U, 0U));
+  EXPECT_EQ(out({0, 0}), jit(div_aa)(0U, 0U, 0U));
+  EXPECT_EQ(out({0, 0}), jit(div_aa)(1'024U, 0U, 0U));
+}
+
+TEST(Day_17_Chronospatial_Computer_JIT_CDV_Register_B) {
+  const auto div_ab = std::vector<uint8_t>{7, 5, 5, 6, 5, 4};
+  EXPECT_EQ(out({0, 7}), jit(div_ab)(7U, 3U, 0U));
+  EXPECT_EQ(out({3, 7}), jit(div_ab)(7U, 1U, 0U));
+  EXPECT_EQ(out({3, 1}), jit(div_ab)(25U, 3U, 0U));
+  EXPECT_EQ(out({4, 0}), jit(div_ab)(4'096U, 10U, 0U));
+}
+
+TEST(Day_17_Chronospatial_Computer_JIT_CDV_Register_C) {
+  const auto div_ac = std::vector<uint8_t>{7, 6, 5, 6, 5, 4};
+  EXPECT_EQ(out({0, 7}), jit(div_ac)(7U, 0U, 3U));
+  EXPECT_EQ(out({3, 7}), jit(div_ac)(7U, 0U, 1U));
+  EXPECT_EQ(out({3, 1}), jit(div_ac)(25U, 0U, 3U));
+  EXPECT_EQ(out({4, 0}), jit(div_ac)(4'096U, 0U, 10U));
+}
+
+//
 // Complete sample programs
 //
 
@@ -128,5 +271,13 @@ TEST(Day_17_Chronospatial_Computer_JIT_Sample) {
   const auto ra  = 729U;
   const auto src = std::vector<uint8_t>{0, 1, 5, 4, 3, 0};
   const auto ret = std::vector<uint8_t>{4, 6, 3, 5, 6, 3, 5, 2, 1, 0};
+  EXPECT_EQ(encode3Bit(ret), jit(src)(ra, 0U, 0U));
+}
+
+TEST(Day_17_Chronospatial_Computer_JIT_Challenge) {
+  const auto ra = 37283687U;
+  const auto src =
+      std::vector<uint8_t>{2, 4, 1, 3, 7, 5, 4, 1, 1, 3, 0, 3, 5, 5, 3, 0};
+  const auto ret = std::vector<uint8_t>{1, 5, 3, 0, 2, 5, 2, 5, 3};
   EXPECT_EQ(encode3Bit(ret), jit(src)(ra, 0U, 0U));
 }
--- 17/day_17_vm.hh
+++ 17/day_17_vm.hh
@@ -6,6 +6,7 @@
 
 #include <array>
 #include <cstdint>
+#include <optional>
 #include <vector>
 
 namespace Day17::Detail {
@@ -36,23 +37,65 @@ namespace Day17 {
 
 enum OpCode : uint8_t { ADV = 0x00, BXL, BST, JNZ, BXC, OUT, BDV, CDV };
 
-void emit_ADV(uint8_t arg, auto&& emit) {
-  if (arg < 4) {
-    const auto dd = static_cast<uint8_t>(arg & 0x7);
+enum Register : uint8_t { RA = 0x04, RB, RC };
+
+[[nodiscard]] constexpr auto isLiteral(uint8_t operand) -> bool {
+  return operand < 4U;
+}
+
+[[nodiscard]] constexpr auto literal(uint8_t operand) -> uint8_t {
+  return static_cast<uint8_t>(operand & 0x7);
+}
+
+// The JIT compiler will create a single function
+// with the following signature:
+//
+using chronospatial_computer = size_t (*)(size_t, size_t, size_t);
+
+// On Linux, x86, the following registers are used for
+// this function call
+//   %rdi -> First function parameter
+//           This will represent Register 'A' (short 'RA')
+//   %rsi -> Second function paramter
+//           This will represent Register 'B' (short 'RB')
+//   %rdx -> Second function paramter
+//           This will represent Register 'C' (short 'RC')
+//
+// The return value of this function is passed in this register:
+//   %rax -> Function return value
+//
+// The output is presented as a single size_t (aka uint64_t) value,
+// which represents a string of 3-bit values, with a leading '1' bit.
+// See description of the 'OUT' opcode for details.
+
+//
+// OPCODE 0 -> ADV
+//
+// Divides register RA by 2^COMBO(RA), where COMBO() is either a
+// 3-bit literal or the value of RA, RB or RC.
+//
+// The ADV, BDV and CDV functions all use an intermediate register:
+//   %rcx -> Temprarily holds value to be used in shift-right
+//
+// This temporary register is used, since the 64-bit input registers
+// cannot be used directly in the SHR instruction.
+//
+void emit_ADV(uint8_t operand, auto&& emit) {
+  if (isLiteral(operand)) {
     emit(std::array<uint8_t, 4>{
-        0x48, 0xc1, 0xef, dd,  // shr    $0x3,%rdi
+        0x48, 0xc1, 0xef, literal(operand),  // shr    <op>,%rdi
     });
-  } else if (arg == 4) {
+  } else if (operand == Register::RA) {
     // Special case for RA / 2^RA: This always result in 0
     emit(std::array{
         0x48, 0x31, 0xff  // xor    %rdi,%rdi
     });
-  } else if (arg == 5) {
+  } else if (operand == Register::RB) {
     emit(std::array{
         0x48, 0x89, 0xf1,  // mov    %rsi,%rcx
         0x48, 0xd3, 0xef   // shr    %cl,%rdi
     });
-  } else if (arg == 6) {
+  } else if (operand == Register::RC) {
     emit(std::array{
         0x48, 0x89, 0xd1,  // mov    %rdx,%rcx
         0x48, 0xd3, 0xef   // shr    %cl,%rdi
@@ -60,19 +103,126 @@ void emit_ADV(uint8_t arg, auto&& emit) {
   }
 }
 
-void emit_BXL(uint8_t arg, auto&& emit) {
-  const auto dd = static_cast<uint8_t>(arg & 0x7);
+//
+// OPCODE 6 -> BDV
+//
+// Same as ADV (see above).
+// Result is stored in RB.
+//
+void emit_BDV(uint8_t operand, auto&& emit) {
+  if (isLiteral(operand)) {
+    emit(std::array<uint8_t, 7>{
+        0x48, 0x89, 0xfe,                    // mov    %rdi,%rsi
+        0x48, 0xc1, 0xee, literal(operand),  // shr    <op>,%rsi
+    });
+  } else if (operand == Register::RA) {
+    // Special case for RA / 2^RA: This always result in 0
+    // ... which in the BDV case means we clear out RB
+    emit(std::array{
+        0x48, 0x31, 0xf6  // xor    %rsi,%rsi
+    });
+  } else if (operand == Register::RB) {
+    emit(std::array{
+        0x48, 0x89, 0xf1,  // mov    %rsi,%rcx
+        0x48, 0x89, 0xfe,  // mov    %rdi,%rsi
+        0x48, 0xd3, 0xee   // shr    %cl,%rsi
+    });
+  } else if (operand == Register::RC) {
+    emit(std::array{
+        0x48, 0x89, 0xd1,  // mov    %rdx,%rcx
+        0x48, 0x89, 0xfe,  // mov    %rdi,%rsi
+        0x48, 0xd3, 0xee   // shr    %cl,%rsi
+    });
+  }
+}
+
+//
+// OPCODE 7 -> CDV
+//
+// Same as ADV (see above).
+// Result is stored in RC.
+//
+void emit_CDV(uint8_t operand, auto&& emit) {
+  if (isLiteral(operand)) {
+    emit(std::array<uint8_t, 7>{
+        0x48, 0x89, 0xfa,                   // mov    %rdi,%rdx
+        0x48, 0xc1, 0xea, literal(operand)  // shr    <op>,%rdx
+    });
+  } else if (operand == Register::RA) {
+    // Special case for RA / 2^RA: This always result in 0
+    // ... which in the CDV case means we clear out RC
+    emit(std::array{
+        0x48, 0x31, 0xd2  // xor    %rdx,%rdx
+    });
+  } else if (operand == Register::RB) {
+    emit(std::array{
+        0x48, 0x89, 0xf1,  // mov    %rsi,%rcx
+        0x48, 0x89, 0xfa,  // mov    %rdi,%rdx
+        0x48, 0xd3, 0xea   // shr    %cl,%rdx
+    });
+  } else if (operand == Register::RC) {
+    emit(std::array{
+        0x48, 0x89, 0xd1,  // mov    %rdx,%rcx
+        0x48, 0x89, 0xfa,  // mov    %rdi,%rdx
+        0x48, 0xd3, 0xea   // shr    %cl,%rdx
+    });
+  }
+}
+
+//
+// OPCODE 1 -> BXL
+//
+// Bitwise XOR of RB and the literal operand.
+//
+void emit_BXL(uint8_t operand, auto&& emit) {
   emit(std::array<uint8_t, 4>{
-      0x48, 0x83, 0xf6, dd  // xor    $dd, %rsi
+      0x48, 0x83, 0xf6, literal(operand)  // xor    <op>, %rsi
   });
 }
 
-void emit_BXC(uint8_t /*unused*/, auto&& emit) {
+//
+// OPCODE 2 -> BST
+//
+// Store the value of COMBO(operand) in RB.
+//
+void emit_BST(uint8_t operand, auto&& emit) {
+  if (isLiteral(operand)) {
+    emit(std::array<uint8_t, 7>{
+        0x48, 0xc7, 0xc6, literal(operand), 0x00, 0x00, 0x00  // mov <dd>,%rsi
+    });
+  } else if (operand == Register::RA) {
+    emit(std::array{
+        0x48, 0x89, 0xfe  // mov    %rdi,%rsi
+    });
+  } else if (operand == Register::RB) {
+    // Nothing to do. RB is already in RB; just mod8 below...
+
+  } else if (operand == Register::RC) {
+    emit(std::array{
+        0x48, 0x89, 0xd6  // mov    %rdx,%rsi
+    });
+  }
+
+  // Always mod 8 in the end...
   emit(std::array{
-      0x48, 0x31, 0xd6  // xor    %rdx,%rsi
+      0x48, 0x83, 0xe6, 0x07  // and    $0x7,%rsi
   });
 }
 
+//
+// OPCODE 3 -> JNZ
+//
+// Jumps to the given instruction, identified by the
+// absolute instruction counter given by the operand,
+// if the value in RA is NOT zero.
+//
+// NOTE:
+// This only emits the JNZ instruction code assembly.
+// The actual (relative) x86 instruction address to jump
+// to is back-patched later, once the instruction address
+// is known.
+// The x86_64 JNZ instruction is given a signed, 8-bit
+// relative jump address.
 void emit_JNZ(uint8_t /*unused*/, auto&& emit) {
   emit(std::array<uint8_t, 5>{
       0x48, 0x85, 0xff,  // test   %rdi,%rdi
@@ -80,39 +230,62 @@ void emit_JNZ(uint8_t /*unused*/, auto&& emit) {
   });
 }
 
-void emit_OUT(uint8_t arg, auto&& emit) {
-  if (arg >= 4) {
-    const auto from_reg = std::array<uint8_t, 3>{
-        0xf8,  // RDI
-        0xf0,  // RSI
-        0xd0   // RDX
+//
+// OPCODE 4 -> BCX
+//
+// Bitwise XOR of RB and RC.
+// The result is stored in RB
+//
+// The operand is unused.
+//
+void emit_BXC(uint8_t /*unused*/, auto&& emit) {
+  emit(std::array{
+      0x48, 0x31, 0xd6  // xor    %rdx,%rsi
+  });
+}
 
-    };
-    const auto dd = from_reg[arg - 4];
-    emit(std::array<uint8_t, 14>{
-        0x49, 0xc1, 0xe1, 0x03,  // shl    $0x3,%r9
-        0x49, 0x89, dd,          // mov    <dd>,%r8
-        0x49, 0x83, 0xe0, 0x07,  // and    $0x7,%r8
-        0x4d, 0x09, 0xc1,        // or     %r8,%r9
+//
+// OPCODE 5 -> OUT
+//
+// Adds the 3-bit value identified by COMBO(operand) to the
+// return value of the function.
+//
+// The OUT instruction uses an additional intermediate
+// register:
+//     %r8 - Tempoarily stores the value to be output, in
+//           order to perform a "mod 8" operation first,
+//           since OUT outputs 3 bits at a time.
+//
+void emit_OUT(uint8_t operand, auto&& emit) {
+  if (isLiteral(operand)) {
+    emit(std::array<uint8_t, 8>{
+        0x48, 0xc1, 0xe0, 0x03,             // shl    $0x3,%rax
+        0x48, 0x83, 0xc8, literal(operand)  // or     <op>,%rax
     });
 
   } else {
-    emit(std::array<uint8_t, 8>{
-        0x49, 0xc1, 0xe1, 0x03,  // shl    $0x3,%r9
-        0x49, 0x83, 0xc9, arg    // or     $0x3,%r9
+    const auto ra_rb_rc = std::array<uint8_t, 3>{0xf8, 0xf0, 0xd0};
+    const auto from     = ra_rb_rc[operand - 4];
+    emit(std::array<uint8_t, 14>{
+        0x48, 0xc1, 0xe0, 0x03,  // shl    $0x3,%rax
+        0x49, 0x89, from,        // mov    <from>,%r8
+        0x49, 0x83, 0xe0, 0x07,  // and    $0x7,%r8
+        0x4c, 0x09, 0xc0,        // or     %r8,%rax
     });
   }
 }
 
-auto compile(auto&& program, auto* x86_it) -> bool {
+auto compile(auto&& program, auto* x86_it) -> std::optional<size_t> {
+  const auto before = x86_it;
+
   auto emit = [&](auto&& rng) mutable {
     x86_it = std::copy(std::begin(rng), std::end(rng), x86_it);
   };
 
-  // Setting %R9 to 1 initially as there might be leading zeros in the output.
+  // Setting %rax to 1 initially as there might be leading zeros in the output.
   // This 1 needs to be removed from the final output before use.
   emit(std::array{
-      0x49, 0xc7, 0xc1, 0x01, 0x00, 0x00, 0x00  // mov $0x1,%r9
+      0x48, 0xc7, 0xc0, 0x01, 0x00, 0x00, 0x00  // mov $0x1,%rax
   });
 
   auto jump_label = std::vector<uint8_t*>{};
@@ -120,54 +293,32 @@ auto compile(auto&& program, auto* x86_it) -> bool {
 
   auto in_it = std::begin(program);
   while (in_it != std::end(program)) {
-    if (jump_label.size() < 8) jump_label.emplace_back(x86_it);
+    // Remember jump-to address since it might be needed to back-patch
+    // a JNZ instruction later...
+    jump_label.emplace_back(x86_it);
 
-    const auto op  = *in_it++;
-    const auto arg = *in_it++;
-    switch (op) {
+    const auto opcode  = *in_it++;
+    const auto operand = *in_it++;
+    switch (opcode) {
         // clang-format off
-      case OpCode::ADV: emit_ADV(arg, emit); break;
-      case OpCode::BXL: emit_BXL(arg, emit); break;
-      case OpCode::JNZ: emit_JNZ(arg, emit); jumps.emplace_back(x86_it, arg); break;
-      case OpCode::BXC: emit_BXC(arg, emit); break;
-      case OpCode::OUT: emit_OUT(arg, emit); break;
+      case OpCode::ADV: emit_ADV(operand, emit); break;
+      case OpCode::BXL: emit_BXL(operand, emit); break;
+      case OpCode::BST: emit_BST(operand, emit); break;
+      case OpCode::JNZ: emit_JNZ(operand, emit); jumps.emplace_back(x86_it, operand); break;
+      case OpCode::BXC: emit_BXC(operand, emit); break;
+      case OpCode::OUT: emit_OUT(operand, emit); break;
+      case OpCode::BDV: emit_BDV(operand, emit); break;
+      case OpCode::CDV: emit_CDV(operand, emit); break;
         // clang-format on
 
-      case OpCode::CDV: {
-        if (arg == 5) {
-          emit(std::array{
-              0x48, 0x89, 0xf1,  // mov    %rsi,%rcx
-              0x48, 0x89, 0xfa,  // mov    %rdi,%rdx
-              0x48, 0xd3, 0xea   // shr    %cl,%rdx
-          });
-        } else {
-          fmt::print("Unhandled arg for op {} ({})\n", op, arg);
-          return false;
-        }
-        break;
-      }
-
-      case OpCode::BST:
-        if (arg == 4) {
-          emit(std::array{
-              0x48, 0x89, 0xfe,       // mov    %rdi,%rsi
-              0x48, 0x83, 0xe6, 0x07  // and    $0x7,%rsi
-          });
-        } else {
-          fmt::print("Unhandled arg for op {} ({})\n", op, arg);
-          return false;
-        }
-        break;
-
       default:
-        fmt::print("Unhandled command {} ({})\n", op, arg);
-        return false;
+        fmt::print("Unhandled command {} ({})\n", opcode, operand);
+        return std::nullopt;
     }
   }
 
   emit(std::array{
-      0x4c, 0x89, 0xc8,  // mov    %r9,%rax
-      0xc3               // ret
+      0xc3  // ret
   });
 
   // Back-patch jump labels
@@ -176,10 +327,8 @@ auto compile(auto&& program, auto* x86_it) -> bool {
         -(back_patch_at - jump_label.at(jump_label_index)));
   }
 
-  return true;
+  return std::distance(before, x86_it);
 }
-
-using chronospatial_computer = size_t (*)(size_t, size_t, size_t);
 
 template <size_t SIZE = 1'024>
 [[nodiscard]] auto jit(auto&& program) -> chronospatial_computer {
